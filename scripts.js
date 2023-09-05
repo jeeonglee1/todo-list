5887fdc5bbda6fd47e410d164d2a53adefebd247
jeeonglee1 <jeeonglee1@gmail.com>
2023-09-05 20:57:50 -0300

    feat: console log input value

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
+const button = document.querySelector(".button-add-task");
+const input = document.querySelector(".input-task");
 
+function pegarValorDoInput() {
+  console.log(input.value);
+}
+
+button.addEventListener("click", pegarValorDoInput);
