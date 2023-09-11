c6f34a49437317169a8be9151270f5b6ba2949da
jeeonglee1 <jeeonglee1@gmail.com>
2023-09-11 20:07:37 -0300

    feat: add multiple tasks

--- scripts.js
+++ scripts.js
@@ -1,12 +1,31 @@
 const button = document.querySelector(".button-add-task");
 const input = document.querySelector(".input-task");
+const fullList = document.querySelector(".list-task");
 
 let minhaListaDeTarefas = [];
 
 function adicionarNovaTarefa() {
   minhaListaDeTarefas.push(input.value);
+
+  mostrarTarefas();
 }
 
-function mostrarTarefas() {}
+function mostrarTarefas() {
+  let novaLi = "";
+
+  minhaListaDeTarefas.forEach((tarefa) => {
+    novaLi =
+      novaLi +
+      `
+      <li class="task">
+        <img src="./img/checked.png" alt="checked-na-tarefa" />
+        <p>${tarefa}</p>
+        <img src="./img/trash.png" alt="tarefa-para-lixo" />
+      </li>
+      `;
+  });
+
+  fullList.innerHTML = novaLi;
+}
 
 button.addEventListener("click", adicionarNovaTarefa);
