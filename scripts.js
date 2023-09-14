cebb9f53a549502e91ae3fd147970f0bd499f13e
jeeonglee1 <jeeonglee1@gmail.com>
2023-09-13 22:32:17 -0300

    feat: add delete and finish task

--- scripts.js
+++ scripts.js
@@ -1,31 +1,52 @@
 const button = document.querySelector(".button-add-task");
 const input = document.querySelector(".input-task");
-const fullList = document.querySelector(".list-task");
+const listaCompleta = document.querySelector(".list-task");
 
-let minhaListaDeTarefas = [];
+let minhaListaDeItens = [];
 
+// Função de adicionar task
 function adicionarNovaTarefa() {
-  minhaListaDeTarefas.push(input.value);
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false,
+  });
 
   mostrarTarefas();
 }
 
+// Função de mostrar tarefas
 function mostrarTarefas() {
   let novaLi = "";
 
-  minhaListaDeTarefas.forEach((tarefa) => {
+  minhaListaDeItens.forEach((item, index) => {
     novaLi =
       novaLi +
       `
-      <li class="task">
-        <img src="./img/checked.png" alt="checked-na-tarefa" />
-        <p>${tarefa}</p>
-        <img src="./img/trash.png" alt="tarefa-para-lixo" />
+      <li class="task ${item.concluida && "done"}">
+        <img src="./img/checked.png" alt="checked-na-tarefa" onclick="concluirTarefa(${index})"/>
+        <p>${item.tarefa}</p>
+        <img src="./img/trash.png" alt="tarefa-para-lixo" onclick="deletarItem(${index})"/>
       </li>
       `;
   });
 
-  fullList.innerHTML = novaLi;
+  listaCompleta.innerHTML = novaLi;
 }
 
+// Função para concluir tarefa
+function concluirTarefa(index) {
+  minhaListaDeItens[index].concluida = !minhaListaDeItens[index].concluida;
+
+  mostrarTarefas();
+}
+
+// Função para deletar a task + mostrar a lista atualizada
+function deletarItem(index) {
+  minhaListaDeItens.splice(index, 1);
+  console.log(`deletado item ${index}`);
+
+  mostrarTarefas();
+}
+
+// Evento de adicionar no botão
 button.addEventListener("click", adicionarNovaTarefa);
